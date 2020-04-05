a2acea88de9bb3dff342e967783433d2d45fbec5
jdluong <luongjohnd@gmail.com>
2020-04-04 20:30:22 -0700

    replaced servlet backend w/ springboot backend; reinit'd frontend directory

---
+++ fabflix-backend/src/main/java/com/fabflix/fabflix/FabflixApplication.java
@@ -0,0 +1,13 @@
+package com.fabflix.fabflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabflixApplication.class, args);
+    }
+
+}
