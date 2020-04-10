e8fb77ea2daa99af2dcbf82ab8d19a29f8ba7406
jdluong <luongjohnd@gmail.com>
2020-04-09 22:13:32 -0700

    fixed url for api call

--- fabflix-frontend/src/app/services/movie.service.ts
+++ fabflix-frontend/src/app/services/movie.service.ts
@@ -13,7 +13,7 @@ import { Rating } from '../models/Rating';
 })
 export class MovieService {
 
-  private url:string = "http://localhost:8080/fabflix-backend/api/";
+  private url:string = "http://ec2-54-68-162-171.us-west-2.compute.amazonaws.com:8080/fabflix-backend/api/";
 
   constructor(private http: HttpClient) {}
 
