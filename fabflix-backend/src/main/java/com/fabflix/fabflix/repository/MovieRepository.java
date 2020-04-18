9094aacb00ad9e8428c17ab9e3796c168951e1f5
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-04-17 18:16:24 -0700

    refactored jdbc files and fixed login component

--- fabflix-backend/src/main/java/com/fabflix/fabflix/repository/MovieRepository.java
+++ fabflix-backend/src/main/java/com/fabflix/fabflix/repository/MovieRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.fabflix.fabflix.*;
 import org.springframework.web.bind.annotation.PathVariable;
 
-public interface MovieListRepository {
+public interface MovieRepository {
     // find top 20 movie ratings
     List<Rating> findTopTwenty();
 
