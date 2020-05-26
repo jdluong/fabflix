d7e79f2b6e63dd18661d946b0568643c62063657
jdluong <luongjohnd@gmail.com>
2020-05-25 19:50:37 -0700

    changed cors

--- fabflix-backend/src/main/java/com/fabflix/fabflix/repository/JdbcMovieRepository.java
+++ fabflix-backend/src/main/java/com/fabflix/fabflix/repository/JdbcMovieRepository.java
@@ -33,7 +33,8 @@ import java.util.Map;
 
 @RestController
 // @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080", "http://http://ec2-54-68-162-171.us-west-2.compute.amazonaws.com:8080"}, allowCredentials = "true")
-@CrossOrigin(origins = {"*"})
+@CrossOrigin(origins = {"http://98.149.57.70:8080", "http://98.149.57.70:*"}, allowCredentials = "true")
+// @CrossOrigin(origins = {"*"})
 @Repository
 public class JdbcMovieRepository implements MovieRepository {
 
