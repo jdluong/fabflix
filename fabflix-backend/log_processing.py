230d83ca8487cfdfe5ccb4f918e163f9dc2c288b
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-06-05 23:27:13 -0700

    updated script

--- fabflix-backend/log_processing.py
+++ fabflix-backend/log_processing.py
@@ -1,16 +1,19 @@
-log = open("src/main/resources/time_log.txt")
+import sys
+
+file_name = sys.argv[1]
+log = open(str(file_name))
 lines = log.readlines()
 totalTS = 0
 totalTJ = 0
-numTimes = len(lines)
+num_times = len(lines)
 
 for line in lines:
     times = line.split(' ')
     totalTS += int(times[0])
     totalTJ += int(times[1])
 
-averageTS = (totalTS / numTimes) / 1000000
-averageTJ = totalTJ / numTimes / 1000000
+averageTS = (totalTS / num_times) / 1000000
+averageTJ = totalTJ / num_times / 1000000
 
 print('Average Search Time: %.2f ms' % averageTS)
 print('Average JDBC Time: %.2f ms' % averageTJ)
