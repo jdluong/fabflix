5127d417a3881c04b967d3ae1b87d498627c72a0
Ubuntu <ubuntu@ip-172-31-30-168.us-west-2.compute.internal>
2020-06-09 03:22:07 +0000

    fixed url's again

--- fabflix-frontend/src/app/services/server.ts
+++ fabflix-frontend/src/app/services/server.ts
@@ -1,7 +1,7 @@
 export enum ServerIP {
     // CHANGE READ TO CURRENT SERVER
     Read = "https://54.68.162.171:8443",
-    Write = "http://54.68.162.171:8443"
+    Write = "https://54.68.162.171:8443"
     // Read = "http://ec2-54-201-208-32.us-west-2.compute.amazonaws.com:8080/fabflix-backend",
     // Write = "http://ec2-54-201-208-32.us-west-2.compute.amazonaws.com:8080/fabflix-backend"
     // Read = 'http://localhost:8080/fabflix_backend_war'
