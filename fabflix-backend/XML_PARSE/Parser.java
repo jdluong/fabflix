0e19b6a07fbe58a8f9a1baebcc9295a99bfd3c97
Ubuntu <ubuntu@ip-172-31-30-168.us-west-2.compute.internal>
2020-06-09 00:09:59 +0000

    fixed URL's

---
+++ fabflix-backend/XML_PARSE/Parser.java
@@ -0,0 +1,21 @@
+package com.fabflix.fabflix;
+
+import java.sql.SQLException;
+
+public class Parser {
+    public static void main(String[] args) {
+        MovieParser movieParser = new MovieParser();
+        StarParser starParser = new StarParser();
+        CastParser castParser = new CastParser();
+
+        try {
+            movieParser.run();
+            starParser.run();
+            castParser.run();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
